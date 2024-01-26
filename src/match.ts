e65ea8a0d10bfe4ec0d074c6a1d2c1a171080753
cezro <hyderz1218@gmail.com>
2024-01-26 23:06:22 +0800

    almost working except for 1

--- src/match.ts
+++ src/match.ts
@@ -2,7 +2,6 @@ export function match(template: string, check: string): boolean {
 	const templateArr = template.toLowerCase().split('')
 	const checkArr = check.toLowerCase().split('')
 
-
 	for (let i = 0; i < templateArr.length; i++) {
 			function checkStats() {
 				console.log(checkArr, `Check Length: ${checkArr.length} Template Step: ${i+1}`)
@@ -28,6 +27,9 @@ export function match(template: string, check: string): boolean {
 			} else if (templateArr[i] !== '*') {
 				return false
 			}
+			if (i === templateArr.length - 1 && templateArr[i] !== '*' && checkArr.length > 0) {
+				return false;
+		}
 			checkStats()
 	}
 	return true
@@ -36,6 +38,5 @@ export function match(template: string, check: string): boolean {
 const print = console.log
 
 const template = "*7263"
-print(match(template, "--s2s7263!"))
-print(match(template, "7263 "))
+print(match(template, "72637263"))
 
